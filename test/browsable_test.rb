c902ea1dc8825d2929f833498fc9c3d574ede6ff
Daniel Kirsch <danishkirel@gmail.com>
2009-06-05 11:55:41 +0200

    testing equalfields

--- test/browsable_test.rb
+++ test/browsable_test.rb
@@ -46,4 +46,51 @@ class BrowsableTest < ActiveSupport::TestCase
     end
     
   end
+
+  context "with three categorized beans" do
+    
+    setup do
+      @first = CategorizedBean.find 1
+      @second = CategorizedBean.find 2
+      @third = CategorizedBean.find 3
+    end
+  
+    context "the first bean" do
+    
+      should "have the third as next bean" do
+        assert_equal @first.next, @third
+      end
+      
+      should "have no previous bean" do
+        assert_nil @first.prev
+      end
+      
+    end
+
+    context "the second bean" do
+    
+      should "have no prev bean" do
+        assert_nil @second.prev
+      end
+
+      should "have no next bean" do
+        assert_nil @second.next
+      end
+      
+    end
+
+    context "the third bean" do
+    
+      should "have the first as prev bean" do
+        assert_equal @third.prev, @first
+      end
+      
+      should "have no next bean" do
+        assert_nil @third.next
+      end
+      
+    end
+    
+  end
+
 end
